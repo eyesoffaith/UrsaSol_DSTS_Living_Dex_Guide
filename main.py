ee4a95cc7f61030745a6449a0ff96cfcb4a826f4
Daniel Miles <miles.daniell009@gmail.com>
2025-11-12 18:02:49 -0500

    updated logic

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ df_chart_column_order = ["from_digimon_id", "to_digimon_id", "from_name", "to_na
 
 df_digi_chart = pl.DataFrame()
 df_digi_count = pl.DataFrame()
-df_digi_tracker = pl.DataFrame()
+df_digi_tracker = pl.DataFrame(schema={"id":pl.Int64, "origin_digimon_id":pl.Int64})
 
 def print_df(df: pl.DataFrame):
     print(''.join([f'{col:<60}' for col in df.columns]))
@@ -26,49 +26,52 @@ def print_df(df: pl.DataFrame):
         print(line)
     print('')
 
-def add_to_digi_count(df_digi: pl.DataFrame):
+# TODO: Finish this method
+# For some reason it's not recording every record for each digimon (i.e. EX-Tyranomon (id #1) should have 5 records (one for itself and it's four pre-digivolutions))
+def add_to_digi_tracker(df_digi: pl.DataFrame):
     global df_digi_tracker
 
-    df_digi = df_digi.group_by("id").agg(
-        pl.col("count").sum(),
-        pl.col("generation").first()
-    )
+    df_digi_tracker = pl.concat([df_digi_tracker, df_digi])
+
+def add_to_digi_count(df_digi: pl.DataFrame):
+    global df_digi_count
+    add_to_digi_tracker(df_digi)
+
+    df_digi = df_digi.group_by("id").agg(pl.len().alias("count"))
     # print("Adding to digi")
     # print_df(df_digi)
 
-    df_digi_tracker = pl.concat([df_digi_tracker, df_digi]).group_by("id").agg(
-        pl.col("count").sum(),
-        pl.col("generation").first()
-    )
+    df_digi_count = pl.concat([df_digi_count, df_digi]).group_by("id").agg(pl.col("count").sum())
     # print("Result")
-    # print_df(df_digi_tracker)
+    # print_df(df_digi_count)
 
 def update_digi_count(df_digi: pl.DataFrame, digi_ids_previous: list[str]=[]):
     # Update digi count
     add_to_digi_count(df_digi)
 
     digi_ids = df_digi["id"].unique().to_list()
-    digi_next_gen = df_digi_chart.filter(pl.col("to_digimon_id").is_in(digi_ids) & ~pl.col("from_digimon_id").is_in(digi_ids) & ~pl.col("from_digimon_id").is_in(digi_ids_previous))\
-                                 .join(df_digi_tracker, left_on="from_digimon_id", right_on="id")\
-                                 .group_by("to_digimon_id")\
-                                 .agg(
-                                    # When choosing which pre-digivolution to choose, choose the one that's appeared most often already
-                                    # i.e. "If you already have to farm a digimon a decent bit, then you already have a good farm for them, farm a few more" typeshit typeshit
-                                    pl.col("from_digimon_id").filter(pl.col("count") == pl.col("count").max()).first().alias("from_digimon_id"),
-                                    pl.col("from_generation").filter(pl.col("count") == pl.col("count").max()).first().alias("from_generation"),
-                                    pl.col("count").max()
-                                 ).sort("to_digimon_id")
 
-    if len(digi_next_gen) == 0:
+    # The filter prevents looping into other digis in the same generation (i.e. Omnimon to Omnimon Zwart) or to a digimon from the previous step (i.e. Ceresmon to Ceresmon Medium)
+    # The "count" here is the count of the "from_digimon" so we can choose the pre-digivolution that has the highest "count"
+    df_digi_next = df_digi.join(df_digi_chart, left_on="id", right_on="to_digimon_id")\
+                          .join(df_digi_count, left_on="from_digimon_id", right_on="id")\
+                          .filter(~pl.col("from_digimon_id").is_in(digi_ids) & ~pl.col("from_digimon_id").is_in(digi_ids_previous))\
+                          .group_by("origin_digimon_id")\
+                          .agg(
+                                # When choosing which pre-digivolution to choose, choose the one that's appeared most often already
+                                # i.e. "If you already have to farm a digimon a decent bit, then you already have a good farm for them, farm a few more" typeshit typeshit
+                                pl.col("from_digimon_id").filter(pl.col("count") == pl.col("count").max()).first().alias("from_digimon_id"),
+                          )\
+                          .rename({"from_digimon_id":"id"})\
+                          .select(["id", "origin_digimon_id"])
+    
+    if len(df_digi_next) == 0:
         return
 
-    df_digi_next = df_digi.join(digi_next_gen, left_on="id", right_on="to_digimon_id")\
-                          .select(["from_digimon_id", "from_generation", "count"])\
-                          .rename({"from_digimon_id": "id", "from_generation":"generation"})
     update_digi_count(df_digi_next, digi_ids)
     
 def main():
-    global df_digi_chart, df_digi_tracker
+    global df_digi_chart, df_digi_count
 
     df_digi_data = pl.read_csv("data/digimon_status_data.csv")
     df_digi_data = df_digi_data.select(df_data_columns.keys()).rename(df_data_columns)
@@ -83,29 +86,27 @@ def main():
     # Handle initial case of gen 1 digimon (In-Training I)
     generation_list.remove(1)
     digi_gen_1 = df_digi_chart.filter(pl.col("from_generation") == 1)\
-                              .select(["from_digimon_id", "from_generation"])\
-                              .unique()\
-                              .with_columns(pl.lit(1).alias("count"))\
-                              .rename({"from_digimon_id":"id", "from_generation":"generation"})
+                              .select("from_digimon_id").unique()\
+                              .with_columns(pl.col("from_digimon_id").alias("origin_digimon_id"))\
+                              .rename({"from_digimon_id":"id"})
     add_to_digi_count(digi_gen_1)
 
     # TODO: Potentially incorrect. Inconsistent values every run???
     for gen in generation_list:
         digi_for_gen = df_digi_chart.filter(pl.col("to_generation") == gen)\
-                                    .select(["to_digimon_id", "to_generation"])\
-                                    .unique()\
-                                    .with_columns(pl.lit(1).alias("count"))\
-                                    .rename({"to_digimon_id":"id", "to_generation":"generation"})
+                                    .select("to_digimon_id").unique()\
+                                    .with_columns(pl.col("to_digimon_id").alias("origin_digimon_id"))\
+                                    .rename({"to_digimon_id":"id"})
         update_digi_count(digi_for_gen)
         
-    df_digi_tracker = df_digi_tracker.join(df_digi_data, on="id", how="left")\
-                                     .select(["id", "name", "count", "generation"])
-    df_digi_tracker = df_digi_tracker.sort(["generation", "count"], descending=[False, True])
+    df_digi_count = df_digi_count.join(df_digi_data, on="id", how="left")
+    df_digi_count = df_digi_count.sort(["generation", "count"], descending=[False, True])
     
-    df_digi_tracker.select(["id", "name", "count"]).write_csv("out.csv")
+    df_digi_count.select(["id", "name", "count"]).write_csv("out.csv")
 
-    print(df_digi_tracker["count"].sum())
-    print_df(df_digi_tracker)
+    print(df_digi_count["count"].sum())
+    print_df(df_digi_count.select(["id", "name", "count", "generation"]))
+    print_df(df_digi_tracker.select(["origin_digimon_id", "id"]).sort("origin_digimon_id"))
 
 if __name__ == "__main__":
     main()
