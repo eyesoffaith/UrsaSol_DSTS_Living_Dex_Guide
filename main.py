65ec93ace3c5f5aa37d0fbcaf8543bf4ef1a9b9a
Daniel Miles <miles.daniell009@gmail.com>
2025-11-15 14:29:18 -0500

    fixed file extraction and added built-in save decryption

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Save files decrypted using:
-#     openssl enc -d -aes-128-ecb -K 33393632373736373534353535383833 -in 0000.bin -out decrypted_save.bin -nopad
 # TODO: Consider re-writting without using DataFrames. Not worth the performance gains likely
 # TODO: Digimon with mode changes between the same generation are not properly recorded (i.e. Ceresmon/Cersmon Medium, Bacchusmon/Bacchusmon DM)
 # TODO: Decrypt live save, check for progress of live dex and adjust needed digimon.
@@ -11,6 +9,11 @@ import os
 import shutil
 import subprocess
 import glob
+import binascii
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 
 df_data_columns = {
     "0": "id",
@@ -26,21 +29,11 @@ df_chart_columns = {
 }
 
 GAME_DIR = "P:\Program Files (x86)\Steam\steamapps\common\Digimon Story Time Stranger"
+CHOSEN_SAVE_FILE = "0000.bin"
 
 # TODO: Fix file extraction
-mvgl_file_paths = ["gamedata/patch.dx11", "gamedata/addcont_17.dx11"]
-mbe_file_paths = ["digimon_status*", "evolution*"]
-csv_file_names = ["*_digimon_status_data.csv", "*_evolution_to.csv"]
-
-for mvgl_path in mvgl_file_paths:
-    for csv_pattern, mbe_pattern in zip(mbe_file_paths, csv_file_names):
-        pattern = f"{mvgl_path}/data/{mbe_pattern}/{mbe_pattern}.mbe/{csv_pattern}"
-        print(pattern)
-        for file_path in glob.glob(pattern):
-            print(file_path)
-
-digimon_status_files = [f"{file_path}/data/000_digimon_status_data.csv" for file_path in mvgl_file_paths]
-digimon_evolution_to_files = [f"{file_path}/data/001_evolution_to.csv" for file_path in mvgl_file_paths]
+mvgl_file_names = ["patch.dx11", "addcont_17.dx11"]
+mbe_patterns = ["digimon_status*", "evolution*"]
 
 fusion_digimon = {"char_DINOBEEMON":23, "char_OMEGAMON":88, "char_SUSANOOMON":104, "char_CHAOSMONVALDURARM":118, "char_EXAMON":215, "char_MILLENNIUMON":230, "char_PAILDRAMON":408, "char_GRACENOVAMON":604, "char_SILPHYMON":720, "char_SHAKKOUMON":723, "char_MASTEMON":748, "char_ALPHAMON_OURYUKEN":766, "char_CHAOSMON":772, "char_SKULLBALUCHIMON_TITAMON":915, "char_ENBARRMON_CRANIAMON":494, "char_OMEGAMON_ZWART":757}
 
@@ -49,14 +42,14 @@ df_digi_chart = pl.DataFrame()
 df_digi_count = pl.DataFrame()
 df_digi_tracker = pl.DataFrame()
 
-def _print_df(df: pl.DataFrame):
+def print_df(df: pl.DataFrame):
     print(''.join([f'{col:<60}' for col in df.columns]))
     for row in df.rows():
         line = ''.join([f'{x or "":<60}' for x in row])
         print(line)
     print('')
 
-def _cleanup_raw_columns(df: pl.DataFrame):
+def cleanup_raw_columns(df: pl.DataFrame):
     df.columns = [re.sub(r".* (\d+)", r"\1", col) for col in df.columns]
     return df
 
@@ -73,11 +66,11 @@ def add_to_digi_count(df_digi: pl.DataFrame):
 
     df_digi = df_digi.group_by("id").agg(pl.len().alias("count"))
     # print("Adding to digi")
-    # _print_df(df_digi)
+    # print_df(df_digi)
 
     df_digi_count = pl.concat([df_digi_count, df_digi]).group_by("id").agg(pl.col("count").sum())
     # print("Result")
-    # _print_df(df_digi_count)
+    # print_df(df_digi_count)
 
 def update_digi_count(df_digi: pl.DataFrame, digi_ids_previous: list[str]=[]):
     # Update digi count
@@ -105,11 +98,11 @@ def update_digi_count(df_digi: pl.DataFrame, digi_ids_previous: list[str]=[]):
 
     update_digi_count(df_digi_next, digi_ids)
     
-def extract_digimon_from_save(filename: str):
+def extract_digimon_from_save(file_path: str):
     regex_save_break = r"[-:+\(\) \w]+"
     regex_digimon_extract = r"[-:+\(\) \w]{3,}mon[-:+\(\) \w]*"
 
-    with open("data/save_complete.bin", encoding='shift_jis', errors="ignore") as file:
+    with open(file_path, encoding='shift_jis', errors="ignore") as file:
         content = file.read()
 
         matches = re.findall(regex_save_break, content)
@@ -117,35 +110,70 @@ def extract_digimon_from_save(filename: str):
         content = "\n".join(content_parts).split("\n".join([f"{n}" for n in range(10)]))[0]
         
         return re.findall(regex_digimon_extract, content)
-        
+
+def decrypt_save(input_file_path: str, output_file_path: str):
+    ENCRYPTION_KEY = "33393632373736373534353535383833"
+    key = binascii.unhexlify(ENCRYPTION_KEY)
+
+    backend = default_backend()
+    # AES-128 in ECB mode
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    decryptor = cipher.decryptor()
+
+    # 3. Read input and decrypt
+    with open(input_file_path, 'rb') as f_in:
+        ciphertext = f_in.read()
+
+    # 4. Perform decryption (ECB is block-wise, so no final block necessary)
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+    # 5. Write output file
+    with open(output_file_path, 'wb') as f_out:
+        f_out.write(decrypted_data)
 
 def main():
     global df_digi_chart, df_digi_count, digi_ids_mode_change
 
-    # Extract files from game dir
-    # for mvgl_path in mvgl_file_paths:
-    #     for mbe_pattern in mbe_file_paths:
-    #         for mbe_file in glob.glob(f"{mvgl_path}/{mbe_pattern}"):
-    #             print(mbe_file)
-
-
     # TODO: Save and uncomment when you can kick it off on game startup
-    # if os.path.exists("./gamedata"):
-    #     shutil.rmtree("./gamedata")
-    for mvgl_path in mvgl_file_paths:
-    #     cmd = ["./MVGLTools/MVGLToolsCLI.exe", "-g", "dsts", "-m", "unpack-mvgl", "-i", f"{GAME_DIR}/{mvgl_path}.mvgl", "-o", f"./{mvgl_path}"]
-    #     subprocess.run(cmd)
-
-        for mbe_pattern in mbe_file_paths:
-            for mbe_file in glob.glob(f"{mvgl_path}/{mbe_pattern}"):
-                cmd = ["./MVGLTools/MVGLToolsCLI.exe", "-g", "dsts", "-m", "unpack-mbe", "-i", f"{mbe_file}", "-o", f"{mbe_file.replace(".mbe", "")}"]
+    # if os.path.exists("./unpacked"):
+    #     shutil.rmtree("./unpacked")
+    for mvgl_file_name in mvgl_file_names:
+        # TODO: Save and uncomment when you can kick it off on game startup
+        # cmd = ["./MVGLTools/MVGLToolsCLI.exe", "-g", "dsts", "-m", "unpack-mvgl", "-i", f"{GAME_DIR}/gamedata/{mvgl_file_name}.mvgl", "-o", f"unpacked/{mvgl_file_name}.mvgl"]
+        # subprocess.run(cmd)
+        for mbe_pattern in mbe_patterns:
+            for file_path in glob.glob(f"unpacked/{mvgl_file_name}.mvgl/data/{mbe_pattern}.mbe"):
+                cmd = ["./MVGLTools/MVGLToolsCLI.exe", "-g", "dsts", "-m", "unpack-mbe", "-i", f"{file_path}", "-o", "unpacked"]
                 subprocess.run(cmd)
+    digimon_status_files = glob.glob(f"unpacked/*/*_digimon_status_data.csv")
+    digimon_evolution_to_files = glob.glob(f"unpacked/*/*_evolution_to.csv")
 
+    # Extract digimon from save data
     df_name_translate = pl.read_csv("data/digi_name_translate.csv")
 
-    df_digi_data = _cleanup_raw_columns(pl.concat([pl.read_csv(file_path) for file_path in digimon_status_files])).select(df_data_columns.keys()).rename(df_data_columns)
+    save_files = [save_file for save_file in glob.glob(f"{GAME_DIR}/gamedata/savedata/*/*.bin") if re.match(r"\d{4}.bin", os.path.basename(save_file))]
+    for file_path in save_files:
+        file_name = os.path.basename(file_path)
+        if not os.path.exists("unpacked/decrypted_saves"):
+            os.mkdir("unpacked/decrypted_saves")
+        decrypt_save(file_path, f"unpacked/decrypted_saves/{file_name}")
+        # cmd = ["openssl", "enc", "-d", "-aes-128-ecb", "-K", "33393632373736373534353535383833", "-in", file_path, "-out", f"unpacked/{file_name}", "-nopad"]
+        # subprocess.run(cmd)
 
-    df_digi_chart = _cleanup_raw_columns(pl.concat([pl.read_csv(file_path) for file_path in digimon_evolution_to_files])).select(df_chart_columns.keys()).rename(df_chart_columns)
+    digis_from_save = extract_digimon_from_save(f"unpacked/decrypted_saves/{CHOSEN_SAVE_FILE}")
+    if len(digis_from_save) == 0:
+        return
+
+    df_digi_from_save = pl.DataFrame({"common_name": digis_from_save}).group_by("common_name")\
+                                                                      .agg(pl.len().alias("count"))\
+                                                                      .join(df_name_translate, on="common_name", how="left")\
+                                                                      .sort("count", descending=[True])
+    print_df(df_digi_from_save)
+
+    # Build evolution data frames
+    df_digi_data = cleanup_raw_columns(pl.concat([pl.read_csv(file_path) for file_path in digimon_status_files])).select(df_data_columns.keys()).rename(df_data_columns)
+
+    df_digi_chart = cleanup_raw_columns(pl.concat([pl.read_csv(file_path) for file_path in digimon_evolution_to_files])).select(df_chart_columns.keys()).rename(df_chart_columns)
     df_digi_chart = df_digi_chart.join(df_digi_data, left_on="from_digimon_id", right_on="id", how="inner").rename({"name": "from_name", "generation": "from_generation"})
     df_digi_chart = df_digi_chart.join(df_digi_data, left_on="to_digimon_id", right_on="id", how="inner").rename({"name": "to_name", "generation": "to_generation"})
 
@@ -155,15 +183,11 @@ def main():
 
     digi_ids_mode_change = df_digi_chart.filter(pl.col("digivolution_type") == 2)["to_digimon_id"].to_list()
 
-    digis_from_save = extract_digimon_from_save("data/save_complete.bin")
-    df_digi_from_save = pl.DataFrame({"common_name": digis_from_save}).group_by("common_name").agg(pl.len().alias("count"))
-    df_digi_from_save = df_digi_from_save.join(df_name_translate, on="common_name", how="left")
-
     # TODO: For viewing and debug, should be removed for release
     digi_from_save_unpaired = df_digi_from_save.filter(pl.col("internal_name").is_null())
     print(f"Unmatched Digimon in Save: {len(digi_from_save_unpaired)}")
     if len(digi_from_save_unpaired) > 0:
-        _print_df(digi_from_save_unpaired)
+        print_df(digi_from_save_unpaired)
 
     generation_list = pl.concat([df_digi_chart["from_generation"], df_digi_chart["to_generation"]]).unique().to_list()
     generation_list.sort()
